51871a08835122bdc221483e69a3a913c0a62502
Muse-zhou <466637566@qq.com>
2019-02-23 17:18:27 +0800

    渲染home

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
-
+import '@/assets/css/base.css'
+import axios from 'axios'
+axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 //所有的Vue插件都是这样用
 //Vue.ues（）
 Vue.use(ElementUI)
