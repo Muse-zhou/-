f6a9db4ab5409bb6bf6051ab48660ed980cb6786
Muse-zhou <466637566@qq.com>
2019-02-28 09:38:40 +0800

    头部调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+        <h2>电商后台管理系</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -11,15 +23,26 @@
 export default {};
 </script>
 <style>
-.container{
-    height: 100%;
-    background-color: red;
+.container {
+  height: 100%;
+  background-color:#b3c0d1;
 }
-.aside{
-    background-color: yellow;
+.aside {
+  background-color: yellow;
 }
-.main{
-    background-color: green ;
+.main {
+  background-color: green;
+}
+.middle{
+  line-height: 60px;
+  text-align: center;
+  color:#fff;
+
+}
+.logout{
+  line-height: 60px;
+  text-decoration: none;
+
 }
 </style>
  
